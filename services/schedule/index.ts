87e289195dbcbf2a609d0d0c74293b055cb74cb0
Meruem117 <1572677319@qq.com>
2022-01-16 18:37:06 +0800

    update schedule service

--- services/schedule/index.ts
+++ services/schedule/index.ts
@@ -2,7 +2,7 @@ import { responseItem } from '@/models/base'
 import { scheduleItem } from '@/models/schedule'
 import { baseUrl } from '@/constant'
 
-export async function getScheduleByQuizId(quizId: number): Promise<responseItem<scheduleItem>> {
+export async function getScheduleListByQuizId(quizId: number): Promise<responseItem<scheduleItem[]>> {
   const response = await fetch(baseUrl + '/schedule/get?quizId=' + quizId)
   return response.json()
 }
@@ -12,17 +12,17 @@ export async function getScheduleList(): Promise<responseItem<scheduleItem[]>> {
   return response.json()
 }
 
-export async function getScheduleStartList(limit: number): Promise<responseItem<scheduleItem[]>> {
-  const response = await fetch(baseUrl + '/schedule/start?limit=' + limit)
+export async function getScheduleStartList(): Promise<responseItem<scheduleItem[]>> {
+  const response = await fetch(baseUrl + '/schedule/start')
   return response.json()
 }
 
-export async function getScheduleEndList(limit: number): Promise<responseItem<scheduleItem[]>> {
-  const response = await fetch(baseUrl + '/schedule/end?limit=' + limit)
+export async function getScheduleEndList(): Promise<responseItem<scheduleItem[]>> {
+  const response = await fetch(baseUrl + '/schedule/end')
   return response.json()
 }
 
-export async function getScheduleRemainList(limit: number): Promise<responseItem<scheduleItem[]>> {
-  const response = await fetch(baseUrl + '/schedule/remain?limit=' + limit)
+export async function getScheduleRemainList(): Promise<responseItem<scheduleItem[]>> {
+  const response = await fetch(baseUrl + '/schedule/remain')
   return response.json()
 }
