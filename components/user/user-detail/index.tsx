ee151a09fc586cf56b3e5cfb52ab4cbf66b1e306
Meruem117 <1572677319@qq.com>
2022-01-29 23:27:05 +0800

    update user detail

--- components/user/user-detail/index.tsx
+++ components/user/user-detail/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Badge, Avatar, Space, Typography } from 'antd'
-import { MailOutlined, ManOutlined, WomanOutlined } from '@ant-design/icons'
+import { MailOutlined, EnvironmentOutlined, CalendarOutlined, ManOutlined, WomanOutlined } from '@ant-design/icons'
 import IconText from '@/components/common/icon-text'
 import type { userItem } from '@/models/user'
 import { ROLE_TYPE, GENDER, GENDER_TEXT, GENDER_AVATAR_SRC } from '@/constant'
@@ -11,7 +11,7 @@ const UserDetail: React.FC<{ data: userItem }> = ({ data }) => {
       <div className="base-box flex">
         <Avatar size={96} src={GENDER_AVATAR_SRC[data.gender]} />
         <div className="flex flex-col p-4">
-          <Space size="small">
+          <Space>
             <Typography.Title level={2}>{data.name}</Typography.Title>
             {
               data.gender === GENDER.Male ?
@@ -19,7 +19,11 @@ const UserDetail: React.FC<{ data: userItem }> = ({ data }) => {
                 <WomanOutlined title={GENDER_TEXT[GENDER.Female]} className="text-red-500 text-base" />
             }
           </Space>
-          <IconText icon={MailOutlined} text={data.email} title={`Email: ${data.email}`} />
+          <Space>
+            {data.location === '' ? <IconText icon={EnvironmentOutlined} text={data.location} title={`Location: ${data.location}`} /> : undefined}
+            <IconText icon={MailOutlined} text={data.email} title={`Email: ${data.email}`} />
+            <IconText icon={CalendarOutlined} text={data.createTime} title={`Created: ${data.createTime}`} />
+          </Space>
         </div>
       </div>
     </Badge.Ribbon>
