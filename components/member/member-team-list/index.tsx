7804fff06b8088ba6a869829f9248532ce8a4da9
Meruem117 <1572677319@qq.com>
2022-02-01 16:25:40 +0800

    update user detail

--- components/member/member-team-list/index.tsx
+++ components/member/member-team-list/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Link from 'next/link'
+import { List, Card, Typography } from 'antd'
 import type { memberItem } from '@/models/member'
 
 const MemberTeamList: React.FC<{ data: memberItem[] }> = ({ data }) => {
   return (
-    <div className="base-box">{data[0].teamName}</div>
+    <List
+      grid={{
+        gutter: 16,
+        xs: 1,
+        sm: 2,
+        md: 2,
+        lg: 3,
+        xl: 3,
+        xxl: 3,
+      }}
+      dataSource={data}
+      renderItem={item => (
+        <List.Item>
+          <Link href={`/team/${item.teamId}`} passHref>
+            <Card title={item.teamName} className="cursor-pointer rounded-md shadow hover:shadow-xl duration-150">
+              <Typography.Text type="secondary">Joined at</Typography.Text> {item.joinTime}
+            </Card>
+          </Link>
+        </List.Item>
+      )}
+    />
   )
 }
 
--- components/user/user-detail/index.tsx
+++ components/user/user-detail/index.tsx
@@ -8,7 +8,7 @@ import { ROLE_TYPE, GENDER, GENDER_TEXT, GENDER_AVATAR_SRC } from '@/constant'
 const UserDetail: React.FC<{ data: userItem }> = ({ data }) => {
   return (
     <Badge.Ribbon text={ROLE_TYPE[data.role].text} color={ROLE_TYPE[data.role].color}>
-      <div className="base-box flex">
+      <div className="flex rounded-lg bg-white shadow p-3">
         <Avatar size={96} src={GENDER_AVATAR_SRC[data.gender]} />
         <div className="flex flex-col p-4">
           <Space>
@@ -19,10 +19,10 @@ const UserDetail: React.FC<{ data: userItem }> = ({ data }) => {
                 <WomanOutlined title={GENDER_TEXT[GENDER.Female]} className="text-red-500 text-base" />
             }
           </Space>
-          <Space>
-            {data.location === '' ? <IconText icon={EnvironmentOutlined} text={data.location} title={`Location: ${data.location}`} /> : undefined}
+          <Space size="middle">
             <IconText icon={MailOutlined} text={data.email} title={`Email: ${data.email}`} />
             <IconText icon={CalendarOutlined} text={data.createTime} title={`Created: ${data.createTime}`} />
+            {data.location === '' ? undefined : <IconText icon={EnvironmentOutlined} text={data.location} title={`Location: ${data.location}`} />}
           </Space>
         </div>
       </div>
