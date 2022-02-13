d76a1a7460083e8d8bf141e43b47885d4ff00d8d
Meruem117 <1572677319@qq.com>
2022-02-13 22:45:30 +0800

    update question paper

--- components/question/question-paper/index.tsx
+++ components/question/question-paper/index.tsx
@@ -1,46 +1,78 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Space, Typography, Radio, List, Button, message, RadioChangeEvent } from 'antd'
 import { cloneDeep } from 'lodash'
 import type { questionItem } from '@/models/question'
 
 const QuestionPaper: React.FC<{ data: questionItem[] }> = ({ data }) => {
   const total = data.length
-  const [index, setIndex] = useState<number>(0)
+  const [current, setCurrent] = useState<number>(0)
   const [answers, setAnswers] = useState<string[]>([])
+  const [tags, setTags] = useState<boolean[]>([])
 
   const onChange = (e: RadioChangeEvent) => {
     const currentAnswers = cloneDeep(answers)
-    currentAnswers[index] = e.target.value
+    currentAnswers[current] = e.target.value
     setAnswers(currentAnswers)
   }
 
   const onNext = () => {
-    if (index + 1 < total) {
-      setIndex(index + 1)
+    if (current + 1 < total) {
+      setCurrent(current + 1)
     } else {
       message.info('This is the last question')
     }
+  }
+
+  const onTag = () => {
+    const currentTags = cloneDeep(tags)
+    currentTags[current] = currentTags[current] ? !currentTags[current] : true
+    setTags(currentTags)
   }
 
   const onSubmit = () => {
     console.log(answers)
   }
 
+  const NumberButton = ({ index }: { index: number }) => {
+    let classes = 'w-9 h-9 p-1 text-base text-center cursor-pointer shadow-md bg-white'
+
+    if (index === current) {
+      classes += ' border border-solid border-gray-400'
+    }
+    if (answers[index] && !tags[index]) {
+      classes += ' bg-green-300'
+    }
+    if (tags[index]) {
+      classes += ' bg-yellow-300'
+    }
+
+    return (
+      <div
+        onClick={() => setCurrent(index)}
+        className={classes}
+        title={`${index + 1}`}
+      >
+        {index + 1}
+      </div>
+    )
+  }
+
   return (
     <div className="base-x-container">
       <div className="base-box w-3/4">
         <div>
-          <Typography.Title level={3}>{data[index].question}</Typography.Title>
-          <Radio.Group onChange={onChange} value={answers[index]} size="large">
+          <Typography.Title level={3}>{current + 1}. {data[current].question}</Typography.Title>
+          <Radio.Group onChange={onChange} value={answers[current]} size="large">
             <Space direction="vertical">
-              <Radio value='a' className="text-lg">A. {data[index].optionA}</Radio>
-              <Radio value='b' className="text-lg">B. {data[index].optionB}</Radio>
-              <Radio value='c' className="text-lg">C. {data[index].optionC}</Radio>
-              <Radio value='d' className="text-lg">D. {data[index].optionD}</Radio>
+              <Radio value='a' className="text-lg">A. {data[current].optionA}</Radio>
+              <Radio value='b' className="text-lg">B. {data[current].optionB}</Radio>
+              <Radio value='c' className="text-lg">C. {data[current].optionC}</Radio>
+              <Radio value='d' className="text-lg">D. {data[current].optionD}</Radio>
             </Space>
           </Radio.Group>
         </div>
         <div className="flex justify-end space-x-4">
+          <Button type="primary" onClick={onTag}>Tag</Button>
           <Button type="primary" onClick={onSubmit}>Submit</Button>
           <Button type="primary" onClick={onNext}>Next</Button>
         </div>
@@ -54,17 +86,17 @@ const QuestionPaper: React.FC<{ data: questionItem[] }> = ({ data }) => {
             md: 3,
             lg: 3,
             xl: 5,
-            xxl: 5,
+            xxl: 5
           }}
           dataSource={data}
           renderItem={item => (
             <List.Item>
-              <Button onClick={() => setIndex(data.indexOf(item))}>{data.indexOf(item) + 1}</Button>
+              <NumberButton index={data.indexOf(item)} />
             </List.Item>
           )}
         />
       </div>
-    </div>
+    </div >
   )
 }
 
--- components/schedule/schedule-title/index.tsx
+++ components/schedule/schedule-title/index.tsx
@@ -4,8 +4,11 @@ import type { scheduleItem } from '@/models/schedule'
 
 const ScheduleTitle: React.FC<{ data: scheduleItem }> = ({ data }) => {
   return (
-    <div className="base-box flex">
+    <div className="base-box flex flex-col">
       <Typography.Title level={4} title={`${data.quizName} - Round ${data.round}`}>{data.quizName} #{data.round}</Typography.Title>
+      <div className="flex">
+        <Typography.Text type="secondary" title={`${data.count} questions in total`}>{data.count} questions</Typography.Text>
+      </div>
     </div>
   )
 }
