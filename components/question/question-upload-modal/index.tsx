57a0181e0216ccef980d19188c4bad440e641af2
Meruem117 <1572677319@qq.com>
2022-05-16 02:36:53 +0800

    update question upload

--- components/question/question-upload-modal/index.tsx
+++ components/question/question-upload-modal/index.tsx
@@ -1,9 +1,49 @@
 import React from 'react'
-import { Modal, Form } from 'antd'
+import { Modal, Form, Input } from 'antd'
+import { questionItem } from '@/models/question'
+import { uploadQuestion } from '@/services/question'
 
 const QuestionUploadModal: React.FC<{ visible: boolean, closeModal: VoidFunction }> = ({ visible, closeModal }) => {
+  const [form] = Form.useForm()
+  const autoSize = { minRows: 1, maxRows: 5 }
+
+  const upload = (): void => {
+    console.log(1)
+  }
+
+  const cancel = (): void => {
+    closeModal()
+    form.resetFields()
+  }
+
   return (
-    <Modal title="Upload Question" visible={visible} onCancel={closeModal}>
+    <Modal title="Upload Question" visible={visible} onCancel={cancel} okText="Upload" onOk={upload} >
+      <Form form={form} layout="vertical">
+        <Form.Item name="question" label="Question">
+          <Input.TextArea autoSize={autoSize} />
+        </Form.Item>
+        <Form.Item name="topic" label="Topic">
+          <Input />
+        </Form.Item>
+        <Form.Item name="type" label="Type">
+          <Input />
+        </Form.Item>
+        <Form.Item name="option_a" label="Option A">
+          <Input.TextArea autoSize={autoSize} />
+        </Form.Item>
+        <Form.Item name="option_b" label="Option B">
+          <Input.TextArea autoSize={autoSize} />
+        </Form.Item>
+        <Form.Item name="option_c" label="Option C">
+          <Input.TextArea autoSize={autoSize} />
+        </Form.Item>
+        <Form.Item name="option_d" label="Option D">
+          <Input.TextArea autoSize={autoSize} />
+        </Form.Item>
+        <Form.Item name="answer" label="Answer">
+          <Input />
+        </Form.Item>
+      </Form>
     </Modal>
   )
 }
