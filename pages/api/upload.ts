d819bf16ff7e1131d14920ce1493ae9118214566
Meruem117 <1572677319@qq.com>
2022-05-18 23:52:04 +0800

    add upload api router

---
+++ pages/api/upload.ts
@@ -0,0 +1,13 @@
+import type { postRequestItem } from '@/models/base'
+import type { questionItem } from '@/models/question'
+import { uploadQuestion } from '@/services/question'
+
+async function uploadHandler(req: postRequestItem<questionItem>, res: any) {
+  if (req.method === 'POST') {
+    const data = req.body
+    const response = await uploadQuestion(data)
+    res.status(200).json(response.data)
+  }
+}
+
+export default uploadHandler
