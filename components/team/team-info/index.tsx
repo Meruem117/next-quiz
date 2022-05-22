939ef3f9e2b2a03859b8276542896d8f30f30554
Meruem117 <1572677319@qq.com>
2022-05-22 16:03:41 +0800

    update team info

--- components/team/team-info/index.tsx
+++ components/team/team-info/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Typography } from 'antd'
 import { UserOutlined, CalendarOutlined } from '@ant-design/icons'
+import { useAppSelector } from '@/app/hooks'
+import { selectUser } from '@/features/user/userSlice'
 import IconText from '@/components/common/icon-text'
 import IconLink from '@/components/common/icon-link'
 import type { teamItem } from '@/models/team'
 
 const TeamInfo: React.FC<{ data: teamItem }> = ({ data }) => {
+  const userState = useAppSelector(selectUser)
+
+  const onApply = async (): Promise<void> => {
+    console.log(11)
+  }
+
   return (
     <div className="base-info">
       <Typography.Title level={3}>{data.name}</Typography.Title>
@@ -17,6 +25,7 @@ const TeamInfo: React.FC<{ data: teamItem }> = ({ data }) => {
         title={data.description}
       >{data.description}
       </Typography.Paragraph>
+      <Typography.Link onClick={onApply}>Apply for the team.</Typography.Link>
     </div>
   )
 }
