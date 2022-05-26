1b2adb1cdbbb128382122a5b0dc186f78390b753
Meruem117 <1572677319@qq.com>
2022-05-26 16:55:02 +0800

    update member apply table

--- components/member/member-apply-table/index.tsx
+++ components/member/member-apply-table/index.tsx
@@ -1,22 +1,31 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
-import { Table, List, Avatar, Typography, Button, Popconfirm, message } from 'antd'
+import { Table, Button, Popconfirm, message } from 'antd'
 import type { ColumnsType } from 'antd/lib/table'
-import { CalendarOutlined } from '@ant-design/icons'
-import { cloneDeep } from 'lodash'
-import IconText from '@/components/common/icon-text'
 import type { memberItem } from '@/models/member'
 import { handlePass } from '@/services/member'
+import { PASS, PASS_TYPE } from '@/constant'
 
 const MemberApplyTable: React.FC<{ data: memberItem[] }> = ({ data }) => {
-  const [visible, setVisible] = useState<boolean>(false)
+  const [passVisible, setPassVisible] = useState<boolean>(false)
+  const [denyVisible, setDenyVisible] = useState<boolean>(false)
+  const [passLoading, setPassLoading] = useState<boolean>(false)
+  const [denyLoading, setDenyLoading] = useState<boolean>(false)
   const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([])
-  const [loading, setLoading] = useState<boolean>(false)
   const columns: ColumnsType<memberItem> = [
-    { title: 'Name', dataIndex: 'userName' },
+    {
+      title: 'Name', dataIndex: 'userName',
+      render: (text: string, record: memberItem, index: number) => (
+        <Link href={`/user/${record.userId}`} passHref>
+          <span title={text} className="cursor-pointer hover:text-blue-600">
+            {text}
+          </span>
+        </Link>
+      )
+    },
     { title: 'Apply Time', dataIndex: 'applyTime' }
   ]
-  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+  const onSelectChange = (newSelectedRowKeys: React.Key[]): void => {
     setSelectedRowKeys(newSelectedRowKeys)
   }
   const rowSelection = {
@@ -25,58 +34,49 @@ const MemberApplyTable: React.FC<{ data: memberItem[] }> = ({ data }) => {
   }
   const hasSelected = selectedRowKeys.length > 0
 
+  const checkMembership = async (pass: string): Promise<void> => {
+    const ids = selectedRowKeys.join(',')
+    pass === PASS.PASS ? setPassLoading(true) : setDenyLoading(true)
+    const res = await handlePass({ ids, pass })
+    if (res.data) {
+      message.info(`${PASS_TYPE[pass].name} successfully`)
+    } else {
+      message.error(`Fail to ${PASS_TYPE[pass].name.toLowerCase()}`)
+    }
+    pass === PASS.PASS ?
+      (setPassLoading(false), setPassVisible(false)) :
+      (setDenyLoading(false), setDenyVisible(false))
+  }
+
   return (
     <div className="flex flex-col space-y-3">
       <div className="flex space-x-3">
         <Popconfirm
           title='Are you sure to pass?'
-          visible={visible}
-          // onConfirm={() => deleteMember(item.id, index)}
-          onCancel={() => setVisible(false)}
+          visible={passVisible}
+          onConfirm={() => checkMembership(PASS.PASS)}
+          onCancel={() => setPassVisible(false)}
         >
-          <Button type="primary" onClick={() => setVisible(true)} disabled={!hasSelected} loading={loading}>Pass</Button>
+          <Button type="primary" onClick={() => setPassVisible(true)} disabled={!hasSelected} loading={passLoading}>
+            Pass
+          </Button>
+        </Popconfirm>
+        <Popconfirm
+          title='Are you sure to deny?'
+          visible={denyVisible}
+          onConfirm={() => checkMembership(PASS.DENIED)}
+          onCancel={() => setDenyVisible(false)}
+        >
+          <Button type="primary" danger onClick={() => setDenyVisible(true)} disabled={!hasSelected} loading={denyLoading}>
+            Deny
+          </Button>
         </Popconfirm>
         <span>
-          {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
+          {hasSelected ? `Selected ${selectedRowKeys.length} users` : ''}
         </span>
       </div>
-      <Table rowSelection={rowSelection} columns={columns} dataSource={data} />
+      <Table rowKey="id" rowSelection={rowSelection} columns={columns} dataSource={data} />
     </div>
-    // <List
-    //   itemLayout="horizontal"
-    //   dataSource={data}
-    //   pagination={{
-    //     pageSize: 6,
-    //   }}
-    //   renderItem={(item, index) => (
-    //     <List.Item
-    //       className="p-2 rounded-md"
-    //       actions={[
-    //         <IconText key={item.id} icon={CalendarOutlined} text={item.joinTime} title={`Joined time: ${item.joinTime}`} />,
-    // <Popconfirm
-    //   key={item.id}
-    //   title='Are you sure to pass?'
-    //   visible={visible[index]}
-    //   // onConfirm={() => deleteMember(item.id, index)}
-    //   onCancel={() => closePopconfirm(index)}
-    // >
-    //   <Button key={item.id} type="link" danger onClick={() => showPopconfirm(index)}>Pass</Button>
-    // </Popconfirm>
-    //       ]}
-    //     >
-    //       <List.Item.Meta
-    //         avatar={<Avatar className="bg-orange-500">{convertUsername(item.userName)}</Avatar>}
-    //         title={
-    //           <Link href={`/user/${item.userId}`} passHref>
-    //             <Typography.Title level={4} title={item.userName} className="cursor-pointer hover:text-blue-600">
-    //               {item.userName}
-    //             </Typography.Title>
-    //           </Link>
-    //         }
-    //       />
-    //     </List.Item>
-    //   )}
-    // />
   )
 }
 
